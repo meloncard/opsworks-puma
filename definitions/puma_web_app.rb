a94eb47479b24d75e8372334ea59c3eb4a7e7e99
Geoff Hayes <hayesgm@gmail.com>
2014-07-02 14:41:33 -0700

    Add todos for checking if we should be careful here in the case of a null domain object

--- definitions/puma_web_app.rb
+++ definitions/puma_web_app.rb
@@ -19,8 +19,8 @@ define :puma_web_app do
   # Enable Nginx web application
   nginx_web_app deploy[:application] do
     docroot deploy[:absolute_document_root]
-    server_name deploy[:domains].first
-    server_aliases deploy[:domains][1, deploy[:domains].size] unless deploy[:domains][1, deploy[:domains].size].empty?
+    server_name deploy[:domains].first # TODO?
+    server_aliases deploy[:domains][1, deploy[:domains].size] unless deploy[:domains][1, deploy[:domains].size].empty? # TODO?
     rails_env deploy[:rails_env]
     mounted_at deploy[:mounted_at]
     ssl_certificate_ca deploy[:ssl_certificate_ca]

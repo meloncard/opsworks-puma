579658f5da49ae95a0ecd1d5b6116cd77653cf6a
Geoff Hayes <hayesgm@gmail.com>
2014-07-02 14:41:41 -0700

    Add a warning, to be removed

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -3,6 +3,8 @@ node[:deploy].each do |application, deploy|
   next unless deploy[:puma] # only deploy puma apps
   next unless deploy[:layers].nil? || ( deploy[:layers] & node[:opsworks][:layers] ).count > 0 # allow layer-by-layer restrictions (custom json can include a "layers" key indicting which layers should deploy this app)
 
+  Chef::Log.warn("Application: #{application}, Deploy: #{deploy.to_json}")
+
   # Create a deploy user
   opsworks_deploy_user do
     deploy_data deploy

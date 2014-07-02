b265f5c7a77367503b0b89b5539480d0e42c8e09
Geoff Hayes <hayesgm@gmail.com>
2014-07-02 14:41:06 -0700

    Add better commenting and fix redundant check

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -2,16 +2,22 @@ node[:deploy].each do |application, deploy|
 
   next unless deploy[:puma] # only deploy puma apps
   next unless deploy[:layers].nil? || ( deploy[:layers] & node[:opsworks][:layers] ).count > 0 # allow layer-by-layer restrictions (custom json can include a "layers" key indicting which layers should deploy this app)
+
+  # Create a deploy user
   opsworks_deploy_user do
     deploy_data deploy
   end
-    opsworks_deploy_dir do
+
+  # Create a deploy directory
+  opsworks_deploy_dir do
     user deploy[:user]
     group deploy[:group]
     path deploy[:deploy_to]
   end
+  
+  # Setup the web application
   puma_web_app do
     application application
     deploy deploy
-  end if deploy[:puma]
+  end
 end

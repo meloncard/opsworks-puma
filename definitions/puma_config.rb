bca18a794a2c851a81428c2c701f516f4de06a31
Geoff Hayes <hayesgm@gmail.com>
2014-07-02 14:34:53 -0700

    Add comments and cleanup puma config

--- definitions/puma_config.rb
+++ definitions/puma_config.rb
@@ -1,11 +1,37 @@
-define :puma_config, :owner => 'deploy', :group => 'nginx', :directory  => nil, :puma_directory => nil, :working_dir => nil, :rackup => nil,
-                     :environment => "production", :daemonize => true, :pidfile => nil, :config_path => nil, :state_path => nil,
-                     :stdout_redirect => nil, :stderr_redirect => nil, :output_append => true,
-                     :quiet => false, :thread_min => 0, :thread_max => 16, :bind => nil, :control_app_bind => nil,
-                     :workers => 0, :activate_control_app => true, :logrotate => true, :exec_prefix => nil,
-                     :config_source => nil, :config_cookbook => nil, :worker_timeout => nil,
-                     :preload_app => false, :prune_bundler => true, :on_worker_boot => nil do
+default_parameters = {
+  owner: 'deploy',
+  group: 'nginx',
+  directory: nil,
+  puma_directory: nil,
+  working_dir: nil,
+  rackup: nil,
+  environment: "production",
+  daemonize: true,
+  pidfile: nil,
+  config_path: nil,
+  state_path: nil,
+  stdout_redirect: nil,
+  stderr_redirect: nil,
+  output_append: true,
+  quiet: false,
+  thread_min: 0,
+  thread_max: 16,
+  bind: nil,
+  control_app_bind: nil,
+  workers: 0,
+  activate_control_app: true,
+  logrotate: true,
+  exec_prefix: nil,
+  config_source: nil,
+  config_cookbook: nil,
+  worker_timeout: nil,
+  preload_app: false,
+  prune_bundler: true,
+  on_worker_boot: nil }
 
+define :puma_config, default_parameters do
+
+  # Set defaults that require parameters
   params[:directory] ||= "/srv/www/#{params[:name]}"
   params[:working_dir] ||= "#{params[:directory]}/current"
   params[:puma_directory] ||= "#{params[:directory]}/shared/puma"
@@ -22,8 +48,10 @@ define :puma_config, :owner => 'deploy', :group => 'nginx', :directory  => nil, 
   params[:config_cookbook] ||= "opsworks-puma"
   params[:worker_timeout] ||= "60"
 
+  # Create group
   group params[:group]
 
+  # Create user
   user params[:owner] do
     action :create
     comment "deploy user"
@@ -42,6 +70,7 @@ define :puma_config, :owner => 'deploy', :group => 'nginx', :directory  => nil, 
     group params[:group] if params[:group]
   end
 
+  # puma.rb
   template params[:name] do
     source params[:config_source]
     path params[:config_path]
@@ -82,7 +111,8 @@ define :puma_config, :owner => 'deploy', :group => 'nginx', :directory  => nil, 
     variables params
   end
 
-  template "#{params[:name]}" do
+  # init.d script
+  template params[:name] do
     source "init.d.sh.erb"
     path "/etc/init.d/#{params[:name]}"
     cookbook "opsworks-puma"
@@ -92,6 +122,7 @@ define :puma_config, :owner => 'deploy', :group => 'nginx', :directory  => nil, 
     variables params
   end
 
+  # We are not currently including logrotate?
   if params[:logrotate]
     logrotate_app params[:name] do
       cookbook "logrotate"

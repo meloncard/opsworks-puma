4b07a0fdb3256efb201afe1b2e86ed9d5066f519
Geoff Hayes <hayesgm@gmail.com>
2014-07-03 13:36:00 -0700

    Only use deploy keys if exist

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,11 +26,11 @@ node[:deploy].each do |application, deploy|
   puma_config application do
     directory deploy[:deploy_to]
     environment deploy[:rails_env]
-    logrotate deploy[:puma][:logrotate]
-    thread_min deploy[:puma][:thread_min]
-    thread_max deploy[:puma][:thread_max]
-    workers deploy[:puma][:workers]
-    worker_timeout deploy[:puma][:worker_timeout]
+    logrotate deploy[:puma][:logrotate] if deploy[:puma][:logrotate]
+    thread_min deploy[:puma][:thread_min] if deploy[:puma][:thread_min]
+    thread_max deploy[:puma][:thread_max] if deploy[:puma][:thread_max]
+    workers deploy[:puma][:workers] if deploy[:puma][:workers]
+    worker_timeout deploy[:puma][:worker_timeout] if deploy[:puma][:worker_timeout]
   end
 end
 
